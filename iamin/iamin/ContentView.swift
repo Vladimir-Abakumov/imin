fec97afacdc2aa152299402c15aff7e69ea8b46f
Vladimir Abakumov <vab@trackman.com>
2022-07-17 13:06:10 +0200

    Added a project

---
+++ iamin/iamin/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iamin
+//
+//  Created by Vladimir Abakumov on 17/07/2022.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iamin/iamin/iaminApp.swift
@@ -0,0 +1,18 @@
+//
+//  iaminApp.swift
+//  iamin
+//
+//  Created by Vladimir Abakumov on 17/07/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct iaminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
